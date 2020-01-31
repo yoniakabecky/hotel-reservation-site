9487a0897700716b9980590854917faaab5ea90e
Yoni <yoniakabecky@gmail.com>
2020-01-31 16:19:21 +0800

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>Test</>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:type" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+export class SingleRoom extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        hello from single room page
+      </div>
+    )
+  }
+}
+
+export default SingleRoom;
