a6f1017366fc1caada199e11b53e0c110e89a0de
Yoni <yoniakabecky@gmail.com>
2020-02-05 08:41:25 +0800

    Room List

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Room from '../components/Room';
 
-const RoomList = props => {
+const RoomList = ({ rooms }) => {
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>unfortunately no rooms matched your search parameters</h3>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      This is room list
-    </div>
+    <section className="roomsList">
+      <div className="roomsList-center">
+        {rooms.map(room => {
+          return <Room key={room.id} room={room} />;
+        })}
+      </div>
+    </section>
   );
 }
 
 RoomList.propTypes = {
-
+  rooms: PropTypes.array
 };
 
 export default RoomList;
